6c8d61f2080a938a6f25e18ef52a4035d1eb583e
antonwass <anton_wass@hotmail.com>
2015-12-06 23:42:36 +0100

    bugfix med ui uppdatering från AddMediaStage

--- src/databas_lab_1/AddMediaTypeStage.java
+++ src/databas_lab_1/AddMediaTypeStage.java
@@ -5,7 +5,12 @@
  */
 package databas_lab_1;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -13,8 +18,17 @@ import javafx.stage.Stage;
  */
 public class AddMediaTypeStage extends Stage {
     private DatabaseCommunication dbCom = null;
+    private TextField tf_title = new TextField();
     
     public AddMediaTypeStage(){
+        super.initStyle(StageStyle.DECORATED);
+        VBox vb = new VBox();
         
+        vb.getChildren().add(new Label("Add creator:"));
+        vb.getChildren().add(new Label("Add creator:"));
+        
+        Scene scene_AddMedia = new Scene(vb, 300, 300);
+        
+        super.setScene(scene_AddMedia);
     }
 }
